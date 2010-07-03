cacf9311c70ada51443e47239b2b5bd7caca0778
Lincoln de Sousa <lincoln@minaslivre.org>
2010-07-02 21:08:13 -0300

    Adding the first pieces of this atom implementation
    We currently implement the following objects:
     * Link
     * Content
     * Person
     * SimpleElement (intended to extend other elements)

---
+++ atom.js
@@ -0,0 +1,178 @@
+/* atom.js - Implementation of the Atom Syndication Format (RFC 4287)
+ *
+ * Copyright (C) 2010  Lincoln de Sousa <lincoln@comum.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Public objects in the `atom' namespace */
+
+var atom = {
+    SimpleElement: function (name, value) {
+        this.name = name;
+        this.value = value;
+    },
+
+    Link: function (href) {
+        this.href = href;
+        this.title = null;
+        this.rel = null;
+    },
+
+    Content: function (type) {
+        this.type = type;
+        this.src = null;
+        this.content = null;
+    },
+
+    Person: function (name) {
+        this.name = name;
+        this.email = email;
+        this.iri = iri;
+    }
+
+};
+
+/* Helper functions */
+function isValidString(val) {
+    return typeof(val) === typeof('') && val !== '';
+}
+
+/* Base element for all Atom elements */
+
+function BasicElement () {
+}
+
+BasicElement.prototype = {
+    _getElement: function () {
+        throw new Error('BasicElement._getElement: This method should ' +
+                        'be overrided;')
+    },
+
+    toXML: function () {
+        var element = this._getElement();
+        return element;
+    },
+
+    toString: function () {
+        return this.toXML().outerHTML;
+    }
+};
+
+atom.SimpleElement.prototype = {
+    /* inheritance */
+    __proto__: BasicElement.prototype,
+
+    /* Element Creation */
+    _getElement: function () {
+        var element = document.createElement(this.name);
+        var elementValue = document.createTextNode(this.value);
+        element.appendChild(elementValue);
+        return element;
+    }
+};
+
+atom.Link.prototype = {
+    /* inheritance */
+    __proto__: BasicElement.prototype,
+
+    /* getters and setters */
+    getHref: function () { return this.href; },
+    setHref: function (href) { this.href = href; },
+    getTitle: function () { return this.title; },
+    setTitle: function (title) { this.title = title; },
+    getRel: function () { return this.rel; },
+    setRel: function (rel) { this.rel = rel; },
+
+    /* Element Creation */
+    _getElement: function () {
+        var element = document.createElement('link');
+        if (!isValidString(this.href))
+            throw new Error("Link._getElement: `href' attribute is not set");
+        element.setAttribute('href', this.href);
+        if (isValidString(this.title))
+            element.setAttribute('title', this.title);
+        if (isValidString(this.rel))
+            element.setAttribute('rel', this.rel);
+        return element;
+    }
+};
+
+atom.Content.prototype = {
+    /* inheritance */
+    __proto__: BasicElement.prototype,
+
+    /* getters and setters */
+    getType: function () { return this.type; },
+    setType: function (type) { this.type = type; },
+    getSrc: function () { return this.src; },
+    setSrc: function (src) { this.src = src; },
+    getContent: function () { return this.content; },
+    setContent: function (content) { this.content = content; },
+
+    /* Element Creation */
+    _getElement: function () {
+        var element = document.createElement('content');
+
+        if (!isValidString(this.type))
+            throw new Error("Content._getElement: `type' attr is not set");
+        element.setAttribute('type', this.type);
+
+        if (isValidString(this.src) && isValidString(this.content))
+            throw new Error("Content._getElement: `content' and `src' should " +
+                            "not be set");
+        else if (!isValidString(this.src) && !isValidString(this.content))
+            throw new Error("Content._getElement: `content' OR `src' should" +
+                            "be set");
+
+        if (isValidString(this.src))
+            element.setAttribute('src', this.src);
+        else {
+            var content = document.createTextNode(this.content);
+            element.appendChild(content);
+        }
+        return element;
+    }
+};
+
+atom.Person.prototype = {
+    /* inheritance */
+    __proto__: BasicElement.prototype,
+
+    /* getters and setters */
+    getName: function () { return this.name; },
+    setName: function (name) { this.name = name; },
+    getEmail: function () { return this.email; },
+    setEmail: function (email) { this.email = email; },
+    getIri: function () { return this.iri; },
+    setIri: function (iri) { this.iri = iri; },
+
+    /* Element Creation */
+    _getElement: function () {
+        var element = document.createElement('person');
+        if (this.email) {
+            var email = document.createElement('email')
+            email.appendChild(document.createTextNode(this.email));
+            element.appendChild(email);
+        }
+
+        if (this.iri) {
+            var iri = document.creannteElement('iri')
+            iri.appendChild(document.createTextNode(this.iri));
+            element.appendChild(iri);
+        }
+
+        return element;
+    }
+};
