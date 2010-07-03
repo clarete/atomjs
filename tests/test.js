3be6fdf3cd96ca979fb38ac3557d67990596e226
Lincoln de Sousa <lincoln@minaslivre.org>
2010-07-02 23:17:31 -0300

    Adding tests to atom.SimpleElement and atom.Link objects

--- tests/test.js
+++ tests/test.js
@@ -24,5 +24,39 @@ test('B prototype should inherit all A methods', function () {
     function B () {};
     B.inherits(A);
 
-    ok((new B()).getColor() === 'red');
+    equals((new B()).getColor(), 'red');
+});
+
+test('It should be possible to create simple key/val elements', function () {
+    var selement = new atom.SimpleElement('key', 'val');
+    equals(selement.toString(), '<key>val</key>');
+});
+
+test('It should be possible to extend simple elements', function () {
+    var selement = new atom.SimpleElement('k', 'v');
+    equals(selement.toString(), '<k>v</k>');
+    selement.append(new atom.SimpleElement('sub', 'subv'));
+    equals(selement.toString(), '<k>v<sub>subv</sub></k>');
+});
+
+test('Link objects can have href, title and rel attrs', function () {
+    var link = new atom.Link();
+    try {
+        link.toString();
+        ok(false, "should not be reached");
+    } catch (e) {
+        ok(true, "href attr is required");
+    }
+
+    link.setHref('http://gnome.org');
+
+    equal(link.toString(), '<link href="http://gnome.org">');
+
+    link.setTitle('Link Title');
+    equal(link.toString(), '<link href="http://gnome.org" ' +
+          'title="Link Title">');
+
+    link.setRel('alternate');
+    equal(link.toString(), '<link href="http://gnome.org" ' +
+          'title="Link Title" rel="alternate">');
 });
